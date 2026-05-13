ac1bcbc56a156c1cf14385b3bdb7a51da3392d1a
momofall221 <yz2qqkrpty@privaterelay.appleid.com>
2026-05-14 00:05:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,6 @@ def forcer_vert():
     return jsonify({"status": "erreur", "message": "voie inconnue"}), 400
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, debug=False, host='0.0.0.0', port=port)
