a6ed83ea2328538361016cb90b1354a707c83f26
momofall221 <yz2qqkrpty@privaterelay.appleid.com>
2026-05-15 09:44:13 +0100

    Delete download_cert.py

--- download_cert.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://test.mosquitto.org/ssl/mosquitto.org.crt"
-response = requests.get(url)
-
-with open("mosquitto_ca.crt", "wb") as f:
-    f.write(response.content)
-
-print("✅ Certificat téléchargé : mosquitto_ca.crt")
